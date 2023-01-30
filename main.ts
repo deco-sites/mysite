0ad60c21ba265493e03acb4027e846f69a8cd01a
gimenes <tlgimenes@gmail.com>
2023-01-30 14:30:20 -0300

    vendor deno

--- main.ts
+++ main.ts
@@ -7,12 +7,12 @@ import manifest from "./fresh.gen.ts";
 import { start } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
-import prefetchPlugin from "prefetch";
+// import prefetchPlugin from "prefetch";
 import partytownPlugin from "partytown/mod.ts";
 
 await start(manifest, {
   plugins: [
-    prefetchPlugin(),
+    // prefetchPlugin(),
     partytownPlugin(),
     twindPlugin({
       selfURL: new URL("./twind.config.ts", import.meta.url).href,
---
+++ vendor/esm.sh/twind@0.16.17.js
@@ -0,0 +1,2 @@
+// @deno-types="https://esm.sh/v103/twind@0.16.17/twind.d.ts"
+export * from "./twind@0.16.17.proxied.js";
